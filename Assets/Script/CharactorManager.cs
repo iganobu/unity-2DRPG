2a5e0cb85e0b2ac3d956cf01939497e90cba1d03
igarashi nobuyuki <nobujojo.0501@hotmail.co.jp>
2014-11-29 10:44:01 +0900

    charactetManagerの追加

---
+++ Assets/Script/CharactorManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterManager : MonoBehaviour 
+{
+    public static CharacterManager characterManager;
+    public string                  CP_Name;
+    public int CP_Level;
+    public int CP_EXP;
+    public int CP_HP;
+    public int CP_MaxHP;
+    public int CP_MP;
+    public int CP_MaxMP;
+    public int CP_ParamName;
+    public int CP_ParamPower;
+    public int CP_ParamSpeed;
+    public int CP_ParamIntelligence;
+
+    public void OnEnable() 
+    {
+        if( characterManager == null ){
+            characterManager = this;
+        }
+    }
+
+	void Awake () {
+        DontDestroyOnLoad( this );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
